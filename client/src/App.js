0a7bb87390af61ee556d342f6417fd5089054954
bradleyplunkett <brad.plunkett@datadoghq.com>
2023-03-09 14:22:17 -0700

    changed the image

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={'https://ci6.googleusercontent.com/proxy/pPyzjMxFAl7wxsJE9FGDIRIkNqJryUY8OtJfbaWqXWr4PfW_4O-ql0uh0AkgwSv5ffb0zz3WBDrDXFIFElp1wdYSWyEeMtLY_ZxKTICrmPJPUVn7xzlqAb1xXF4N833iFpjYIfSAQmYBTudAQkMYdEoYnufnomxdpSiQ45lu7RpCiJWmXysQ60LFTmFOb3dpGQ=s0-d-e1-ft#https://ring.widen.net/content/w8ezn64p20/png/GPCS-509_Price_Protection_Hero.png?position=c&color=ffffffff&quality=80&u=vuzezy'} className="App-logo" alt="logo" />
                     <h1 className="App-title">Five Points Security Systems</h1>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
