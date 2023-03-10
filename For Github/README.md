2f9f73168f79096db65ec5f26716e472a4accdca
bradleyplunkett <brad.plunkett@datadoghq.com>
2023-03-10 11:18:29 -0700

    updated

---
+++ For Github/README.md
@@ -0,0 +1,22 @@
+## Tutorial used to create this app
+You are very welcome to check the tutorial on Medium. Just follow the link below.
+- [Create a React FrontEnd, a Node/Express BackEnd and connect them together](https://medium.com/@jrshenrique/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c)
+
+## About the app
+There are two separated apps. The Client which serves the FrontEnd (using React), and the API (in Node/Express).
+
+## How to run the API
+1. In your terminal, navigate to the `api` directory.
+2. Run `npm install` to install all dependencies.
+3. Run `npm start` to start the app.
+
+## How to run the Client
+1. In another terminal, navigate to the `client` directory.
+2. Run `npm install` to install all dependencies.
+3. Run `npm start` to start the app
+
+## Check if they are connected
+1. With the two apps running, open your browser in http://localhost:3000/.
+2. If you see a webpage saying `Five Points Security Systems`, it means the FrontEnd is working.
+3. If the same webpage has the phrase `Affordable Solutions to Protect your Home in the Mile High City!`, it means the API is working.
+# FivePointsSecuritySystems
---
+++ For Github/client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={'https://ci6.googleusercontent.com/proxy/pPyzjMxFAl7wxsJE9FGDIRIkNqJryUY8OtJfbaWqXWr4PfW_4O-ql0uh0AkgwSv5ffb0zz3WBDrDXFIFElp1wdYSWyEeMtLY_ZxKTICrmPJPUVn7xzlqAb1xXF4N833iFpjYIfSAQmYBTudAQkMYdEoYnufnomxdpSiQ45lu7RpCiJWmXysQ60LFTmFOb3dpGQ=s0-d-e1-ft#https://ring.widen.net/content/w8ezn64p20/png/GPCS-509_Price_Protection_Hero.png?position=c&color=ffffffff&quality=80&u=vuzezy'} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Five Points Security Systems</h1>
+                </header>
+                <p className="App-intro">{this.state.apiResponse}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
